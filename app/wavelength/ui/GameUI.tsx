0f8da65288abd4d8afb41fa34ecc42d1963d5dfc
Andre Camerino <camerinoandre@gmail.com>
2026-04-02 00:42:14 +1300

    add simple selected word UI

--- app/wavelength/ui/GameUI.tsx
+++ app/wavelength/ui/GameUI.tsx
@@ -19,44 +19,70 @@ const GameUI = () => {
 
   const handleStartGame = () => {
     setGame(new WavelengthGame(players));
-    // refreshPlayers();
-    console.log(game);
-    console.log(game.getCurrentCategory());
+    setIsShowingTargetNumber(false);
   };
 
   const handleEndGame = () => {
-  game.handleRoundPoints();
-  setPlayers([...game.getPlayers()]); // spread creates a new array reference
-};
+    game.handleRoundPoints();
+    setPlayers([...game.getPlayers()]); // spread creates a new array reference
+    setIsShowingTargetNumber(true);
+  };
 
   return (
     <div className="flex flex-col gap-1 items-start">
       <h1>WAVELENGTH</h1>
       <h2>Category: {game.getCurrentCategory()}</h2>
-      <h2>
-        Target Number: {isShowingTargetNumber ? game.getCurrentTarget() : "___"}
-      </h2>
 
-      <button onClick={() => setIsShowingTargetNumber(!isShowingTargetNumber)}>
-        Toggle Target Number
-      </button>
+      <div className="flex flex-row gap-2">
+        <h2>
+          Target Number:{" "}
+          {isShowingTargetNumber ? game.getCurrentTarget() : "___"}
+        </h2>
+
+        <button
+          onClick={() => setIsShowingTargetNumber(!isShowingTargetNumber)}
+        >
+          {isShowingTargetNumber ? "Hide" : "Show"} Target Number
+        </button>
+      </div>
 
       {players.length > 0 && <h2>Host: {game.getHost().getName()}</h2>}
 
-      {players.map((player) => (
-        <div key={player.getId()}>
-          <p>
-            {player.getName()}: {player.getScore()}
-          </p>
-          <input
-            type="number"
-            min={1}
-            max={10}
-            onChange={(e) => player.setSelectedNumber(Number(e.target.value))}
-            placeholder="Select number"
-          />
-        </div>
-      ))}
+      <div className="flex flex-row gap-2">
+        <p>Selected Word: </p>
+        <input
+          type="text"
+          // value={name}
+          onChange={(e) => game.setSelectedWord(e.target.value)}
+          placeholder="Input Here"
+        />
+      </div>
+
+      <div className="flex flex-col gap-4 py-4">
+        {players.map((player) => (
+          <div key={player.getId()} className="flex flex-col">
+            <p>
+              {player.getName()}: Points: {player.getScore()}
+            </p>
+            {game.getHost().getName() == player.getName() ? (
+              "Selecting Word (Host)"
+            ) : (
+              <div className="flex flex-row gap-2">
+                <p>Number Guess: </p>
+                <input
+                  type="number"
+                  min={1}
+                  max={10}
+                  onChange={(e) =>
+                    player.setSelectedNumber(Number(e.target.value))
+                  }
+                  placeholder="Select number"
+                />
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
 
       <button
         onClick={() => {
