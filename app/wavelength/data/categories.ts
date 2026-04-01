d41030b4cb3d1e39e2423ecc4572416a2b4aa089
Andre Camerino <camerinoandre@gmail.com>
2026-04-01 16:58:33 +1300

    add basic wavelength categories

---
+++ app/wavelength/data/categories.ts
@@ -0,0 +1,14 @@
+export const categories: string[] = [
+  "Ice Cream Flavours",
+  "Seasons",
+  "Pizza Toppings",
+  "Holiday Destinations",
+  "Superhero Teams",
+  "Snack Brands",
+  "Vacation Styles",
+  "Sports Teams",
+  "Movie Genres",
+  "Board Game Preferences",
+  "Coffee vs Tea",
+  "Fast Food Chains",
+];
