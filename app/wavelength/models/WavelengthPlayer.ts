a9f620459d8e414052398628f83312503d767efe
Andre Camerino <camerinoandre@gmail.com>
2026-04-01 16:59:51 +1300

    add WavelengthPlayer model

---
+++ app/wavelength/models/WavelengthPlayer.ts
@@ -0,0 +1,26 @@
+import { Player } from "@/models/Player";
+
+export class WavelengthPlayer extends Player {
+  private score: number = 0;
+  private selectedNumber: number = 0;
+
+  constructor(id: string, name: string) {
+    super(id, name);
+  }
+
+  addScore(score: number) {
+    this.score += score;
+  }
+
+  setSelectedNumber(selection: number) {
+    this.selectedNumber = selection;
+  }
+
+  getScore() {
+    return this.score;
+  }
+
+  getSelectedNumber() {
+    return this.selectedNumber;
+  }
+}
