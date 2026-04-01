602209944d30eb3210de3f0ff3b80b63ab282484
Andre Camerino <camerinoandre@gmail.com>
2026-04-01 16:59:06 +1300

    add basic game logic methods

---
+++ app/wavelength/logic/gameLogic.ts
@@ -0,0 +1,35 @@
+import { getRandomInt } from "@/utils/getRandom";
+import { categories } from "../data/categories";
+
+const MIN_NUMBER = 1;
+const MAX_NUMBER = 10;
+/**
+ * Returns a random number between 1 and 10 (inclusive).
+ * @returns {number} A random integer between min and max
+ */
+export function getRandomNumber(): number {
+  return getRandomInt(MIN_NUMBER, MAX_NUMBER);
+}
+
+const SCORE_MULTIPLIER = 1;
+export function getGuessPoints(targetNumber: number, selectedNumber: number): number {
+  const difference = Math.abs(targetNumber - selectedNumber);
+  return difference < 2 ? (3 - difference) * SCORE_MULTIPLIER : 0;
+}
+
+/**
+ * Returns points based on the average of total player points
+ */
+export function getHostPoints(playerPoints: number[]): number {
+  if (playerPoints.length === 0) return 0;
+
+  const total = playerPoints.reduce((sum, p) => sum + p, 0);
+  const average = total / playerPoints.length;
+
+  return Math.round(average);
+}
+
+export function getCategory(): string {
+  const randomIndex = getRandomInt(0, categories.length - 1);
+  return categories[randomIndex];
+}
