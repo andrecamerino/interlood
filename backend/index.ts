ffbf413373dbcf4ba8682a15af2871628602fcc6
Andre Camerino <camerinoandre@gmail.com>
2026-04-03 14:23:08 +1300

    get players to get added through frontend on 3000 to server on 3002

--- backend/index.ts
+++ backend/index.ts
@@ -1,12 +1,36 @@
-import express from 'express';
-import { createServer } from 'node:http';
+import express from "express";
+import { createServer } from "node:http";
+import { Server } from "socket.io";
 
 const app = express();
 const server = createServer(app);
-const PORT = 3001
+const PORT = 3002;
+export const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000", // your React port
+  },
+});
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+const players: string[] = [];
+
+app.get("/", (req, res) => {
+  res.send(`
+    <h1>Players: ${players.length}</h1>
+    <ul>
+      ${players.map((p) => `<li>${p}</li>`).join("")}
+    </ul>
+  `);
+});
+// TODO: implement emit and recieve updates on player adds on frontend
+
+io.on("connection", (socket) => {
+  console.log("client connected:", socket.id); // add this
+
+  socket.on("user join", (name) => {
+    console.log(`${name} joined the room`);
+    players.push(name);
+    io.emit("players updated", players);
+  });
 });
 
 server.listen(PORT, () => {
