534327e640789279617dae078d3e9bb88c332f5b
Andre Camerino <camerinoandre@gmail.com>
2026-04-03 20:06:49 +1300

    /backend add RoomManager

---
+++ backend/models/RoomManager.ts
@@ -0,0 +1,31 @@
+import { getRandomRoomId } from "@/utils/getRandom";
+import { Room } from "./Room";
+
+export class RoomManager {
+  private rooms: Map<string, Room> = new Map();
+
+  addRoom(hostId: string): string {
+    let id: string;
+    do {
+      id = getRandomRoomId();
+    } while (this.roomIdExists(id));
+    const room = new Room(id, hostId);
+    this.rooms.set(id, room);
+    return id;
+  }
+
+  getRoom(roomId: string): Room | undefined {
+    if (!this.roomIdExists(roomId)) return;
+    return this.rooms.get(roomId);
+  }
+
+  deleteRoom(roomId: string): boolean {
+    if (!this.roomIdExists(roomId)) return false;
+    this.rooms.delete(roomId);
+    return true;
+  }
+
+  roomIdExists(roomId: string) {
+    return this.rooms.has(roomId);
+  }
+}
