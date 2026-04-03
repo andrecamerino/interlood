b616bf35b7c0a914888f0c52591de5503f1d33f5
Andre Camerino <camerinoandre@gmail.com>
2026-04-03 15:02:18 +1300

    add @backend to frontend tsconfig + update imports

--- frontend/app/wavelength/ui/GameUI.tsx
+++ frontend/app/wavelength/ui/GameUI.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { useState } from "react";
-import { WavelengthGame } from "../models/WavelengthGame";
-import { WavelengthPlayer } from "../../../../backend/models/wavelength/WavelengthPlayer";
+import { WavelengthGame } from "@backend/models/wavelength/WavelengthGame";
+import { WavelengthPlayer } from "@backend/models/wavelength/WavelengthPlayer";
 
 const GameUI = () => {
   const [game] = useState<WavelengthGame>(new WavelengthGame([]));
