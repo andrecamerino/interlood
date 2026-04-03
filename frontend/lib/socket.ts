b835f09a28fb7bfb65f16ba6092b56ca75a21aab
Andre Camerino <camerinoandre@gmail.com>
2026-04-03 23:33:08 +1300

    /frontend create socket singleton

---
+++ frontend/lib/socket.ts
@@ -0,0 +1,10 @@
+import { io, Socket } from "socket.io-client";
+
+let socket: Socket;
+
+export const getSocket = (): Socket => {
+  if (!socket) {
+    socket = io("http://localhost:3002");
+  }
+  return socket;
+};
