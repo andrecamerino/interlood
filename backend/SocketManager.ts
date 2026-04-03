3ac443489b8944a37aa4277586ae90be62267738
Andre Camerino <camerinoandre@gmail.com>
2026-04-03 18:45:04 +1300

    /backend add Room to SocketManager

--- backend/SocketManager.ts
+++ backend/SocketManager.ts
@@ -1,10 +1,14 @@
 import { Server, Socket } from "socket.io";
+import { Room } from "./models/Room";
+import { Player } from "@shared/models/Player";
 
 export class SocketManager {
   private io: Server;
+  private room: Room;
 
   constructor(io: Server) {
     this.io = io;
+    this.room = new Room("room-1", "host-1"); // hardcoded for MVP
     this.init();
   }
 
@@ -21,11 +25,25 @@ export class SocketManager {
   }
 
   private onUserJoin(socket: Socket, name: string) {
-    console.log(`${name} joined`);
-    // Room logic will go here
+    const player = new Player(socket.id, name);
+    const added = this.room.addPlayer(player);
+
+    if (!added) {
+      socket.emit("join error", "Name already taken");
+      return;
+    }
+
+    console.log(`${name} joined the room`);
+    this.io.emit("room updated", this.room.toJSON());
   }
 
   private onDisconnect(socket: Socket) {
+    this.room.removePlayer(socket.id);
     console.log("client disconnected:", socket.id);
+    this.io.emit("room updated", this.room.toJSON());
+  }
+
+  getRoom() {
+    return this.room;
   }
 }
